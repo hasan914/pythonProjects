08891d5ba0845b52d1dad0de844f2199a615e70c
hasan914 <hasanjaved667@gmail.com>
2026-06-16 18:00:15 +0500

    list of my friends with some details about them

---
+++ chapter_3_Lists/3.2_greetings.py
@@ -0,0 +1,6 @@
+#created a list of my friends with some details about them
+names=['ali','arslan','umar','ahmad']
+print(names[0].title() + " is my friend of friend.")
+print(names[1].title() + " is my best friend from collage till now.")
+print(names[2].title() + " is my childhood friend and cousin and like a younger brother to me.")
+print(names[3].title() + " is my childhood friend and cousin and like a elder brother to me.")
