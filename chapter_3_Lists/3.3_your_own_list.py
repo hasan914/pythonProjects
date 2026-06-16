d793bc7d39ebc67d09b0d4fceafba5f38168d5b7
hasan914 <hasanjaved667@gmail.com>
2026-06-16 18:12:33 +0500

    printed series of statments about Car List

---
+++ chapter_3_Lists/3.3_your_own_list.py
@@ -0,0 +1,6 @@
+#created a list of car companies
+car=['honda','toyota','suzuki','audi']
+print(car[0].title() + " City is the car which I have right now.")
+print(car[1].title() + " Gli is the car which I have in past.")
+print(car[2].title() + " WagonR is the last car which I sold before buying " + car[0].title() + " City.")
+print(car[3].title() + " is the car which I want to buy in future.")
